d1f6e0e2b769fa834f7cec19156f3d8a23d97797
Lucky <175917645@qq.com>
2016-02-02 17:11:24 +0800

    fill the single product's info

--- views/js/product_detail.js
+++ views/js/product_detail.js
@@ -1,24 +1,25 @@
 $(function() {
-	'use strict';
+    'use strict';
 
-	console.log('123');
-	var url = window.location.pathname;
-	var id = url.substring(url.lastIndexOf('/') + 1);
-    GetJsonData.getJsonNoData('/api/products/' + id, function(res) {
-        console.log(res); 	
+    console.log('123');
+
+
+    $(document).on('pageInit', '#product_detail', function(e, id, page) {
+        $(page).on('click', '.open-about', function() {
+            $.popup('.popup-about');
+        });
+
+        var url = window.location.pathname;
+        var id = url.substring(url.lastIndexOf('/') + 1);
+        GetJsonData.getJsonNoData('/api/products/' + id, function(res) {
+            var data = res.data;
+            $('#p_name').html(data.p_name);
+            $('#p_color').html(data.p_color);
+            $('#p_size').html(data.p_size);
+        })
     })
 
-	$(document).on('pageInit', '#product_detail', function(e, id, page) {
-		$(page).on('click', '.open-about', function() {
-			$.popup('.popup-about');
-		});
-        
-        
-	})
 
-	
 
-	$.init();
+    $.init();
 });
-
-
